72f90b11bf49277b8656122d6693c31ce609a7be
Albertus Marco <99940477+marcoruslie@users.noreply.github.com>
2025-09-15 16:13:53 +0700

    Create 2025_09_15_161205_update_column_detail_qc_po.php

---
+++ database/migrations/2025_09_15_161205_update_column_detail_qc_po.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('detail_qc_po', function (Blueprint $table) {
+            //
+            if (Schema::hasColumn('detail_qc_po', 'po_id')) {
+                $table->renameColumn('po_id', 'id');
+                $table->string('po_id')->references('po_id')->on('list_header_po')->onDelete('cascade');;
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
