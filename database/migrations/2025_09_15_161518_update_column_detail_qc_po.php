5dd81081cc0683fbcb3bfc23545648b8fa275d13
Albertus Marco <99940477+marcoruslie@users.noreply.github.com>
2025-09-15 16:17:45 +0700

    Update 2025_09_15_161518_update_column_detail_qc_po.php

--- database/migrations/2025_09_15_161518_update_column_detail_qc_po.php
+++ database/migrations/2025_09_15_161518_update_column_detail_qc_po.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('detail_qc_po', function (Blueprint $table) {
             //
-            $table->string('po_id')->references('po_id')->on('list_header_po')->onDelete('cascade');
+            $table->string('po_id')->after('id')->references('po_id')->on('list_header_po')->onDelete('cascade');
         });
     }
 
