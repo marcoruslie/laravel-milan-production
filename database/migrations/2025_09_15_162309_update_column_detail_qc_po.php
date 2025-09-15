0ac11c95843a6356faff69d3b8535d898acd63f2
Albertus Marco <99940477+marcoruslie@users.noreply.github.com>
2025-09-15 16:24:27 +0700

    Create 2025_09_15_162309_update_column_detail_qc_po.php

---
+++ database/migrations/2025_09_15_162309_update_column_detail_qc_po.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('detail_qc_po', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('po_id')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
